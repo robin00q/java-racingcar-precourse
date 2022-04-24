9e1a6d5b7ee9b5f2af429b188494e93e90614f44
leesukjune <robin00q@naver.com>
2022-04-24 20:14:48 +0900

    test : ApplicationTest 테스트항목 추가한다

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -5,7 +5,10 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ApplicationTest extends NsTest {
     private static final int MOVING_FORWARD = 4;
@@ -21,6 +24,28 @@ class ApplicationTest extends NsTest {
                 assertThat(output()).contains("pobi : -", "woni : ", "최종 우승자: pobi");
             },
             MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 전진_정지_테스트_2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("lee,suk,june", "2");
+                    assertThat(output()).contains("lee : --", "suk : --", "june : --", "최종 우승자: lee, suk, june");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 전진_정지_테스트_3() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("lee,suk,june", "2");
+                    assertThat(output()).contains("lee : --", "suk : --", "june : ", "최종 우승자: lee, suk");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, STOP, MOVING_FORWARD, MOVING_FORWARD, STOP
         );
     }
 
@@ -42,6 +67,21 @@ class ApplicationTest extends NsTest {
                     assertThat(output()).contains(ERROR_MESSAGE);
                 }
         );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalid_try_count_parameter")
+    void 시도횟수를_올바르지않게_입력한_경우_예외처리(String tryCount) {
+        assertSimpleTest(
+                () -> {
+                    runException("lee,suk,june", tryCount);
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                }
+        );
+    }
+
+    static Stream<String> invalid_try_count_parameter() {
+        return Stream.of("-1", "a", "한글");
     }
 
     @Override
